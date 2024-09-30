fe6f906cbe6c1d77f3c046d25387f965525e2395
Jorge Hernandez <jehernandez199670@gmail.com>
2024-09-29 19:52:01 -0600

    Creando el archivo menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Contacto.cpp"
+#include "listaDobleCircularContactos.cpp"
+#ifndef _MENU
+#define _MENU
+
+
+using namespace std;
+void menuPrincipal(){
+    cout<<"¿Que desea hacer? presione el número correspondiente."<<endl;
+    cout<<"";
+}
+
+
+
+
+
+
+#endif // _MENU
